a84523f1f4e46331f6deba188ba88c5580613ba6
Adrian Luque <alluque@ujaen.es>
2020-12-07 14:52:39 +0100

    Puzzle 6 y 7

---
+++ 6/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _6 {
+    class Program {
+
+        static void Main(string[] args) {
+
+            List<string> groupResponses = new List<string>();
+            int sum = 0;
+
+            parsearString(leerArchivo("input.txt"), groupResponses);
+
+            //////Part 1
+            // foreach (string responses in groupResponses) {
+            //     HashSet<char> responseSet = new HashSet<char>();
+
+            //     foreach(char c in responses){
+            //         if(c!='\n')
+            //             responseSet.Add(c);
+            //     }
+
+            //     sum+=responseSet.Count;
+            // }
+
+            //////Part 2
+            foreach (string responses in groupResponses) {
+                Dictionary<char, int> responseSet = new Dictionary<char, int>();
+                int members = 1;
+
+                foreach (char c in responses) {
+
+                    if (c != '\n') {
+
+                        if (!responseSet.ContainsKey(c))
+                            responseSet.Add(c, 1);
+                        else {
+                            int n = responseSet[c];
+                            responseSet[c] = n + 1;
+                        }
+
+                    } else {
+                        members++;
+                    }
+                }
+                
+                foreach (char key in responseSet.Keys) 
+                    if (responseSet[key] == members)
+                        sum++;
+                
+            }
+
+
+            Console.WriteLine("#Responses: " + sum);
+
+
+        }
+
+        public static void parsearString(string texto, List<string> passports) {
+
+            string[] lineas = texto.Split("\n\n");
+
+            passports.AddRange(lineas);
+
+        }
+
+        public static string leerArchivo(string archivo) {
+
+            return System.IO.File.ReadAllText(archivo);
+
+        }
+    }
+}
---
+++ 7/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _7 {
+    class Program {
+
+        static void Main(string[] args) {
+
+
+            Dictionary<string, Dictionary<string, int>> rules = new Dictionary<string, Dictionary<string, int>>();
+            Dictionary<string, Dictionary<string, int>> rulesInverse = new Dictionary<string, Dictionary<string, int>>();
+
+            parsearString(leerArchivo("input.txt"), rules, rulesInverse);
+
+            List<string> processing = new List<string> { "shiny gold" };
+
+            ////////PARTE 1
+            //HashSet<string> accepted = new HashSet<string>();
+
+            // for (int i = 0; i < processing.Count; i++)
+            //     foreach (string key in rulesInverse[processing[i]].Keys)
+            //         processing.Add(key);
+
+
+            // foreach (string color in processing) 
+            //     accepted.Add(color);
+
+            //Console.WriteLine("Combinations: " + (accepted.Count - 1));
+
+
+            ////////PARTE 2
+            for (int i = 0; i < processing.Count; i++)
+                foreach (string key in rules[processing[i]].Keys)
+                    for (int j = 0; j < rules[processing[i]][key]; j++)
+                        processing.Add(key);
+
+            Console.WriteLine("Combinations: " + (processing.Count - 1));
+
+        }
+
+        public static void parsearString(string texto, Dictionary<string, Dictionary<string, int>> rules, Dictionary<string, Dictionary<string, int>> rulesInverse) {
+
+            string[] lineas = texto.Split("\n");
+
+            foreach (string linea in lineas) {
+                if (linea.Length > 0) {
+
+                    string[] rule = linea.Split(" contain ");
+
+                    string mainColor = rule[0].Remove(rule[0].Length - 5);
+                    rules.Add(mainColor, new Dictionary<string, int>());
+
+                    if (!rulesInverse.ContainsKey(mainColor))
+                        rulesInverse.Add(mainColor, new Dictionary<string, int>());
+
+                    if (rule[1] != "no other bags.") {
+                        string rule2 = rule[1].Remove(rule[1].Length - 1);
+                        string[] subRules = rule2.Split(", ");
+
+                        foreach (string subRule in subRules) {
+                            int capacity = int.Parse(subRule.Split(" ")[0]);
+                            string subColor;
+
+                            if (subRule.EndsWith("bags"))
+                                subColor = subRule.Remove(0, 2).Remove(subRule.Length - 5 - 2);
+                            else
+                                subColor = subRule.Remove(0, 2).Remove(subRule.Length - 4 - 2);
+
+                            rules[mainColor].Add(subColor, capacity);
+
+                            if (!rulesInverse.ContainsKey(subColor))
+                                rulesInverse.Add(subColor, new Dictionary<string, int>());
+
+                            rulesInverse[subColor].Add(mainColor, capacity);
+
+                        }
+                    }
+                }
+            }
+
+            // //Comprobacion
+            // foreach (string mainColor in rules.Keys) {
+
+            //     Console.Write(mainColor);
+
+            //     foreach (string subColor in rules[mainColor].Keys) {
+            //         Console.Write(" | " + rules[mainColor][subColor] + " " + subColor);
+            //     }
+
+            //     Console.WriteLine();
+            // }
+        }
+
+        public static string leerArchivo(string archivo) {
+
+            return System.IO.File.ReadAllText(archivo);
+
+        }
+    }
+}
